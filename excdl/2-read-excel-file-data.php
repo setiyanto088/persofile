88722310681704a0fee47ee1db6a08cf71c1d6b0
setiyanto088 <setiyanto088@gmail.com>
2023-11-04 00:12:54 +0700

    add External Folder

---
+++ excdl/2-read-excel-file-data.php
@@ -0,0 +1,27 @@
+<?php
+//include the file that loads the PhpSpreadsheet classes
+require 'spreadsheet/vendor/autoload.php';
+
+//create directly an object instance of the IOFactory class, and load the xlsx file
+$fxls ='EPG_INRATE_13082023.xls';
+$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($fxls);
+
+$sheetCount = $spreadsheet->getSheetCount();
+
+echo $sheetCount;die;
+
+//read excel data and store it into an array
+$xls_data = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+/* $xls_data contains this array:
+[1=>['A'=>'Domain', 'B'=>'Category', 'C'=>'Nr. Pages'], 2=>['A'=>'CoursesWeb.net', 'B'=>'Web Development', 'C'=>4000], 3=>['A'=>'MarPlo.net', 'B'=>'Courses & Games', 'C'=>15000]]
+*/
+
+//now it is created a html table with the excel file data
+$html_tb ='<table border="1"><tr><th>'. implode('</th><th>', $xls_data[1]) .'</th></tr>';
+$nr = count($xls_data); //number of rows
+for($i=2; $i<=$nr; $i++){
+  $html_tb .='<tr><td>'. implode('</td><td>', $xls_data[$i]) .'</td></tr>';
+}
+$html_tb .='</table>';
+
+echo $html_tb; 
